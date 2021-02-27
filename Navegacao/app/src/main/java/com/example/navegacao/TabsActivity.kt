7a4cfe0dd1183e4216bb95647dfda76e2e5aff70
rruyzz <rodolfo.ruyz@gmail.com>
2021-02-27 18:25:33 -0300

    navegao tab view e bottom

---
+++ Navegacao/app/src/main/java/com/example/navegacao/TabsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.navegacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tabs.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class TabsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabs)
+        setSupportActionBar(toolbar)
+        val pagerAdapter = TabsPagerAdapter(this, supportFragmentManager)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        viewPager.adapter = pagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+}
