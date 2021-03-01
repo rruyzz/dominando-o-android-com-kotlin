295abe7c082b26c3185cd75200feaf4669e77b84
rruyzz <rodolfo.ruyz@gmail.com>
2021-03-01 20:42:24 -0300

    navigationAPi

---
+++ NavigationAPI/app/src/main/java/com/example/navigationapi/CompleteFragment.kt
@@ -0,0 +1,34 @@
+package com.example.navigationapi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_complete.*
+
+
+class CompleteFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_complete, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.run {
+            val fullName = getString("full_name")
+            val age = getInt("age")
+            txtMessage.text = "$fullName - $age"
+        }
+    }
+
+}
